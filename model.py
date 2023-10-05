ce725f0c5a9c968f946c777a5bdb4c54f8a0aac0
Ramiro-Lopez <lopez.j.ramiro1@gmail.com>
2023-10-05 14:55:55 -0500

    add

---
+++ model.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.tsa.api import Holt, ExponentialSmoothing
+from sklearn.metrics import mean_squared_error
+from math import sqrt 
+
+# ==============================================================================================
+
+# evaluation function to compute rmse
+def evaluate(val, yhat_df, target_var):
+    '''
+    This function will take the actual values of the target_var from validate, 
+    and the predicted values stored in yhat_df, 
+    and compute the rmse, rounding to 0 decimal places. 
+    it will return the rmse. 
+    '''
+    val = val
+    yhat_df = yhat_df
+    rmse = round(sqrt(mean_squared_error(val[target_var], yhat_df[target_var])), 0)
+    return rmse
+
+    # evaluation function to compute rmse
+def evaluate(target_var):
+    '''
+    This function will take the actual values of the target_var from validate, 
+    and the predicted values stored in yhat_df, 
+    and compute the rmse, rounding to 0 decimal places. 
+    it will return the rmse. 
+    '''
+    rmse = round(sqrt(mean_squared_error(validate[target_var], yhat_df[target_var])), 0)
+    return rmse
+
+
+# ==============================================================================================
+
+# plot and evaluate 
+def plot_and_eval(train, val, yhat_df, target_var):
+    '''
+    This function takes in the target var name (string), and returns a plot
+    of the values of train for that variable, validate, and the predicted values from yhat_df. 
+    it will als lable the rmse. 
+    '''
+    
+    plt.figure(figsize = (12,4))
+    plt.plot(train[target_var], label = 'Train', linewidth = 1)
+    plt.plot(val[target_var], label = 'Validate', linewidth = 1)
+    plt.plot(yhat_df[target_var])
+    plt.title(target_var)
+    rmse = evaluate(val, yhat_df, target_var)
+    print(target_var, '-- RMSE: {:.0f}'.format(rmse))
+    plt.show()
+
+# ==============================================================================================
+
+
+
+
